59cfc7a2b6f31e8ae8fa520252f681e111acbad9
intankamal2277-ux <intankamal2277@gmail.com>
2026-06-26 16:07:48 +0700

    edit 29 cart controller

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\View\View;
 
-class AuthController extends Controller
+class CartController extends Controller
 {
     /**
      * Show the login form.
